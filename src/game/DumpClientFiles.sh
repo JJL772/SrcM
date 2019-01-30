522bfa8a383b6580ec19558666081f4c59647316
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-01-30 01:11:14 -0800

    Add the first bits of vulkan renderer testing code wooo

---
+++ src/game/DumpClientFiles.sh
@@ -0,0 +1,4 @@
+grep '$File' client/client_base.vpc > client_files.txt
+grep '$File' client/client_hl2mp.vpc > client_files2.txt
+grep '$File' server/server_base.vpc > server_files.txt
+grep '$File' server/server_hl2mp.vpc > server_files2.txt
